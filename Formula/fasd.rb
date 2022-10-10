edeb08e5c97f1abfdb16fca088f70b8d2c7d6b22
Andrew Ferrier <andrewferrier@example.com>
2022-10-10 18:16:01 +0100

    fasd: switch to maintained fork at 1.0.2

--- Formula/fasd.rb
+++ Formula/fasd.rb
@@ -1,16 +1,14 @@
 class Fasd < Formula
   desc "CLI tool for quick access to files and directories"
-  homepage "https://github.com/clvv/fasd"
-  url "https://github.com/clvv/fasd/archive/1.0.1.tar.gz"
-  sha256 "88efdfbbed8df408699a14fa6c567450bf86480f5ff3dde42d0b3e1dee731f65"
+  homepage "https://github.com/whjvenyl/fasd"
+  url "https://github.com/whjvenyl/fasd/archive/refs/tags/1.0.2.tar.gz"
+  sha256 "6e048cfbfe53bf8b806e36db17c4eb2b8e15e8dfaadc759c7ce78e3915716b21"
   license "MIT"
-  head "https://github.com/clvv/fasd.git", branch: "master"
+  head "https://github.com/whjvenyl/fasd", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "9241df0f32971ce5a84c977f6908b93114946843813d5375ba7b983a7a783188"
   end
-
-  deprecate! date: "2022-06-16", because: :repo_archived
 
   def install
     bin.install "fasd"
